3c075f3e91b4c46c1fe167e55907144f26a84cf2
mahmoudhammad97 <43408044+mahmoudhammad97@users.noreply.github.com>
2018-09-20 15:52:24 +0200

    login

--- login-page.js
+++ login-page.js
@@ -8,7 +8,16 @@ function setsize()
 	document.getElementById("main").style.width  = width +"px";
 	document.getElementById("img-background").style.height = height +"px";
 	document.getElementById("img-background").style.width  = width +"px";
-	var toplogin = (height - 293)/2;
-	document.getElementById("loginform").style.Top = toplogin +"px";
-	document.getElementById("").style.width  = (width - 50) +"px";
+	var toplogin = (height - 295)/2;
+	document.getElementById("loginform").style.top = toplogin +"px";
+	var centergoto = (width - 60)/2;
+	document.getElementById("goto").style.left  = centergoto +"px";
+}
+function gotologin()
+{
+	document.getElementById("main2").style.display  = "none";	
+	document.getElementById("main1").style.display  = "block";
+	document.getElementById("main1").style.width = 100 + "%";
+	document.getElementById("main1").style.height = 100 + "%";
+	document.getElementById("main1").style.animationName  = "main1intop";	
 }
